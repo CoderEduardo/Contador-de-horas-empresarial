bb417d59599d6b14f008b7df4d1b63e18fb8568f
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-09-26 18:25:50 -0300

    Conectando o banco de dados

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const PORTA = 8080
+const connection = require("./model/database/database")
+connection.authenticate().then(()=>{console.log("Banco conectado com sucesso")}).catch(erro=>{console.log(`Ocorreu um erro: ${erro}`)})
 const loginRota = require("./routes/login")
 
 app.use("/",loginRota)
