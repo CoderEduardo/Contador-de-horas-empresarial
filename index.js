8b135685f867f131a410fa077e42729ee6d1bdc0
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-09-26 18:18:53 -0300

    Algumas mudanças

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const app = express()
 const PORTA = 8080
+const bodyParser = require("body-parser")
+const ejs = require("ejs")
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+app.set('view engine','ejs')
+app.set(express.static("public"))
 const loginRota = require("./routes/login")
+
 
 app.use("/",loginRota)
 
