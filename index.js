7574224f5b10a9cbfd8d38da5d4ded2b16b17b11
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-09-26 18:33:12 -0300

    Criando a tabela de usuarios

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const PORTA = 8080
 const connection = require("./model/database/database")
 connection.authenticate().then(()=>{console.log("Banco conectado com sucesso")}).catch(erro=>{console.log(`Ocorreu um erro: ${erro}`)})
+const Usuario = require("./model/Usuario/Usuario")
 const loginRota = require("./routes/login")
 
 app.use("/",loginRota)
---
+++ model/Usuario/Usuario.js
@@ -0,0 +1,23 @@
+const Sequelize = require("sequelize")
+const connection = require("../database/database")
+
+const Usuario = connection.define("usuarios",{
+    nome:{
+        type:Sequelize.STRING,
+        allowNull:false
+    },
+    email:{
+        type:Sequelize.STRING,
+        allowNull:false
+    },
+    senha:{
+        type:Sequelize.STRING,
+        allowNull:false
+    },
+    admin:{
+        type:Sequelize.BOOLEAN
+    }
+})
+
+Usuario.sync({force:false})
+module.exports = Usuario
