c6edc63408aae9e2cfe80f36e92580fa3a8edc3c
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-09-26 18:40:43 -0300

    Arrumando os conflitos

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 const PORTA = 8080
+const bodyParser = require("body-parser")
+const ejs = require("ejs")
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+app.set(express.static("public"))
+app.set("view engine","ejs")
+const connection = require("./model/database/database")
+connection.authenticate().then(()=>{console.log("Banco de dados conectado")}).catch(erro=>{console.log(`Ocorreu um erro: ${erro}`)})
 const loginRota = require("./routes/login")
 
 
