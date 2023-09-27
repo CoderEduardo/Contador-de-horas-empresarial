450a5be34b4e939fa77dc609b7d05896516def83
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-09-27 14:41:46 -0300

    Criando a página do funcionário vizualizar os projetos

--- controllers/funcionarioController.js
+++ controllers/funcionarioController.js
@@ -1,7 +1,12 @@
-const funcionarioController = (req,res)=>{
-    res.render("funcionario/index",{
-        admin:req.session.usuario.admin
+const Projeto = require("../model/Projeto/Projeto")
+
+const funcionarioController = async (req, res) => {
+    let projetos = await Projeto.findAll()
+
+    res.render("funcionario/index", {
+        admin: req.session.usuario.admin,
+        projetos:projetos
     })
 }
 
-module.exports = {funcionarioController}
+module.exports = { funcionarioController }
