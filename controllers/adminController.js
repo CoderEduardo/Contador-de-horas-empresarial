cf631c8c8a74aea22662ac1e113c69a9fb1b615c
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-09-28 14:58:25 -0300

    Criando a página para o admin ver os projetos

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,26 +1,29 @@
 const Projeto = require("../model/Projeto/Projeto")
 
-const adminController = (req,res)=>{
-    res.render("admin/index",{
-        admin: req.session.usuario.admin
-    })
+const adminController = async (req, res) => {
+  try {
+    let projetos = await Projeto.findAll()
+    res.render("admin/index", { admin: req.session.usuario.admin,projetos })
+  } catch (erro) {
+    res.redirect('/')
+  }
 }
 
-const cadastrarProjeto = (req,res)=>{
-    res.render("admin/cadastrarProjeto")
+const cadastrarProjeto = (req, res) => {
+  res.render("admin/cadastrarProjeto")
 }
 
-const cadastrar = async (req,res)=>{
-  try{
+const cadastrar = async (req, res) => {
+  try {
     await Projeto.create({
-        nome:req.body.nome,
-        descricao:req.body.descricao,
-        usuarioId:req.session.usuario.id
+      nome: req.body.nome,
+      descricao: req.body.descricao,
+      usuarioId: req.session.usuario.id
     })
     res.redirect("/admin")
-  }catch(erro){
+  } catch (erro) {
     res.sendStatus(400)
   }
 }
 
-module.exports = {adminController, cadastrarProjeto,cadastrar}
+module.exports = { adminController, cadastrarProjeto, cadastrar }
